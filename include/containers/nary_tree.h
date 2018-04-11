ea367150fdccfff6d5b34332974dce6ba7d25f3d
Michael137 <michaelbuch12@gmail.com>
2018-04-11 01:55:50 +0100

    Init commit n-ary tree

---
+++ include/containers/nary_tree.h
@@ -0,0 +1,34 @@
+#ifndef NARY_TREE_H
+#define NARY_TREE_H
+
+// Example N-ary tree
+//
+// Root
+// |
+// |--> Node 1
+// |	|
+// |	|--> Node 1
+// |	|	|
+// |	|	|--> Node 1
+// |	|	...
+// |	|	|--> Node N
+// |	|
+// |	|--> Node 2
+// |	...
+// |	|--> Node N
+// |
+// |--> Node 2
+// ...
+// |--> Node N
+
+// Node
+// - data
+// - debug info
+// - children
+// - ptr to parent
+
+// Tree
+// - depth
+// - root node
+
+#endif // NARY_TREE_H
