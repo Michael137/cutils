ca0c99481e754611081b70fcae085fa446adc161
Michael137 <michaelbuch12@gmail.com>
2018-04-14 10:29:41 +0100

    typelist tweaks

--- include/typelist/typelist.cpp
+++ include/typelist/typelist.cpp
@@ -4,8 +4,18 @@ using namespace tl;
 
 int main()
 {
-	typedef internal::Typelist<char,
-				   internal::Typelist<signed char,
-				   internal::Typelist<unsigned char, internal::NullType>>> CharList;
+	typedef internal::Typelist<
+		char,
+		internal::Typelist<
+			signed char, internal::Typelist<unsigned char, internal::NullType>>>
+		CharList;
+
+	typedef internal::Typelist<
+		signed char,
+		internal::Typelist<
+			short int,
+			internal::Typelist<
+				int, internal::Typelist<long int, internal::NullType>>>>
+		SignedIntegrals;
 	return 0;
 }
--- include/typelist/typelist_internal.h
+++ include/typelist/typelist_internal.h
@@ -1,21 +1,23 @@
 #ifndef TYPELIST_INTERNAL_H_IN
 #define TYPELIST_INTERNAL_H_IN
 
-namespace tl {
-namespace internal {
-
-class NullType {};
-struct EmptyType {};
-
-template<typename T, typename U>
-struct Typelist
+namespace tl
 {
+namespace internal
+{
+
+class NullType
+{
+};
+struct EmptyType {
+};
+
+template<typename T, typename U> struct Typelist {
 	typedef T Head;
 	typedef U Tail;
 };
 
 } // namespace internal
 } // namespace tl
-
 
 #endif // TYPELIST_INTERNAL_H_IN
