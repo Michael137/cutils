8b566627d54a12bf2ea0cec1b753d7ee3ac43b37
Michael Buch <michaelbuch12@gmail.com>
2018-06-03 10:56:57 +0100

    add hash int test

--- test/containers/test_hashmap.c
+++ test/containers/test_hashmap.c
@@ -21,21 +21,54 @@ static bool cmp_fn_str( void const* key, void const* value )
 	return strcmp( (char*)key, (char*)value ) == 0;
 }
 
+static size_t hash_fn_int( void const* num )
+{
+	size_t key = *(int*)num;
+	key = (~key) + (key << 21); // key = (key << 21) - key - 1;
+	key = key ^ (key >> 24);
+	key = (key + (key << 3)) + (key << 8); // key * 265
+	key = key ^ (key >> 14);
+	key = (key + (key << 2)) + (key << 4); // key * 21
+	key = key ^ (key >> 28);
+	key = key + (key << 31);
+	return key;
+}
+
+static bool cmp_fn_int( void const* key, void const* value )
+{
+	return *(int*)key == *(int*)value;
+}
+
 int main()
 {
 	printf( "~~~ Starting Hashmap Tests ~~~\n" );
 
-	HashMap* map;
+	HashMap* str_map;
 
-	hm_create( &map, hash_fn_str, cmp_fn_str );
-	hm_insert( &map, "Key1", "Value1" );
-	hm_insert( &map, "Key2", "Value2" );
-	hm_insert( &map, "Key3", "Value3" );
-	char const* val = hm_get( map, "Key1" );
+	hm_create( &str_map, hash_fn_str, cmp_fn_str );
+	hm_insert( &str_map, "Key1", "Value1" );
+	hm_insert( &str_map, "Key2", "Value2" );
+	hm_insert( &str_map, "Key3", "Value3" );
+	char const* val = hm_get( str_map, "Key1" );
 	assert( strcmp( "Value1", val ) == 0 );
 	printf( "%s\n", val );
 
-	hm_free( map );
+	hm_free( str_map );
+
+	HashMap* int_map;
+
+	hm_create( &int_map, hash_fn_int, cmp_fn_int );
+	int val1 = 1;
+	int val2 = 2;
+	int val3 = 3;
+	hm_insert( &int_map, "Key1", &val1 );
+	hm_insert( &int_map, "Key2", &val2 );
+	hm_insert( &int_map, "Key3", &val3 );
+	int num = *(int*)hm_get( int_map, "Key1" );
+	assert( strcmp( "Value1", val ) == 0 );
+	printf( "%d\n", num );
+
+	hm_free( int_map );
 
 	return 0;
 }
