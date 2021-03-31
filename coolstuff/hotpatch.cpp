7424a18bd95e4b778929b2b549ae030ca440d963
Michael Buch <michaelbuch12@gmail.com>
2021-03-31 01:43:08 +0100

    Add ability to 'un-hotpatch' and demonstrate using two threads

--- coolstuff/hotpatch.cpp
+++ coolstuff/hotpatch.cpp
@@ -1,8 +1,10 @@
 // g++ -pthread hotpatch.cpp && ./a.out
 // clang++ -pthread hotpatch.cpp && ./a.out
 
+#include <atomic>
 #include <cassert>
 #include <chrono>
+#include <condition_variable>
 #include <iostream>
 #include <mutex>
 #include <thread>
@@ -12,6 +14,8 @@
 using namespace std::chrono_literals;
 
 std::mutex g_mtx;
+std::condition_variable g_cv;
+std::atomic<bool> g_shouldPatch{false};
 
 // clang-format off
 #ifdef _WIN32
@@ -37,12 +41,15 @@ void hotpatch( void* target, void* replacement )
 	void* page = reinterpret_cast<void*>( reinterpret_cast<uintptr_t>( target )
 	                                      & ~0xfff );
 
+	// Doesn't work on W^X (write XOR execute) systems
 	mprotect( page, PAGE_SIZE, PROT_WRITE | PROT_EXEC );
 
+	// To account for E9 instruction sub 5 bytes
 	uint32_t rel
 	    = static_cast<char*>( replacement ) - static_cast<char*>( target ) - 5;
 
-	// create x86 jump instruction to replacement address
+	// Create x86 jump instruction to replacement address
+	// Assumes addrs lay within 2gb
 	union {
 		uint8_t data[8];
 		uint64_t alignment;
@@ -50,6 +57,29 @@ void hotpatch( void* target, void* replacement )
 	    = {{0xe9, static_cast<uint8_t>( rel >> 0 ),
 	        static_cast<uint8_t>( rel >> 8 ), static_cast<uint8_t>( rel >> 16 ),
 	        static_cast<uint8_t>( rel >> 24 )}};
+
+	*reinterpret_cast<uint64_t*>( target )
+	    = *reinterpret_cast<uint64_t*>( instruction.data );
+
+	mprotect( page, PAGE_SIZE, PROT_EXEC );
+}
+
+void unpatch( void* target )
+{
+	// Is 8-byte aligned?
+	assert( ( reinterpret_cast<uintptr_t>( target ) & 0x07 ) == 0 );
+
+	void* page = reinterpret_cast<void*>( reinterpret_cast<uintptr_t>( target )
+	                                      & ~0xfff );
+
+	// Doesn't work on W^X (write XOR execute) systems
+	mprotect( page, PAGE_SIZE, PROT_WRITE | PROT_EXEC );
+
+	// Replace hook stub with NOPs
+	union {
+		uint8_t data[8];
+		uint64_t alignment;
+	} instruction = {{0x48, 0x48, 0x48, 0x48, 0x48, 0x48, 0x48, 0x48}};
 
 	*reinterpret_cast<uint64_t*>( target )
 	    = *reinterpret_cast<uint64_t*>( instruction.data );
@@ -72,46 +102,51 @@ int replacement( int num )
 	return num;
 }
 
-void workerCb( int arg )
+__attribute__( ( optimize( "O0" ) ) ) void workerCb( int arg )
 {
-	for( ;; )
+	for( int i = 0;; ++i )
 	{
-		std::lock_guard<std::mutex> lck( g_mtx );
+		std::unique_lock<std::mutex> lck( g_mtx );
 		toHotpatch( arg );
-		std::this_thread::sleep_for( 500ms );
+
+		if( i >= 10 )
+		{
+			lck.unlock();
+			g_cv.notify_one();
+			i = 0;
+		}
+
+		std::this_thread::sleep_for( 100ms );
 	}
 }
 
-void hotpatcherCb( int arg )
+__attribute__( ( optimize( "O0" ) ) ) void hotpatcherCb( int arg )
 {
-	bool toggle = false;
 	for( ;; )
 	{
-		std::lock_guard<std::mutex> lck( g_mtx );
-		std::this_thread::sleep_for( 1s );
-		if( toggle )
+		std::unique_lock<std::mutex> lck( g_mtx );
+		g_cv.wait( lck );
+
+		// Listen for signal
+		if( g_shouldPatch )
 		{
 			hotpatch( reinterpret_cast<void*>( toHotpatch ),
 			          reinterpret_cast<void*>( replacement ) );
+			g_shouldPatch = false;
 		}
 		else
 		{
-			hotpatch( reinterpret_cast<void*>( replacement ),
-			          reinterpret_cast<void*>( toHotpatch ) );
+			unpatch( reinterpret_cast<void*>( toHotpatch ) );
+			g_shouldPatch = true;
 		}
-		toggle ^= true;
 	}
 }
 
 int main()
 {
 	std::thread worker{workerCb, 137};
-	// std::thread hotpatcher{hotpatcherCb, 137};
+	std::thread hotpatcher{hotpatcherCb, 137};
 
-	std::this_thread::sleep_for( 1s );
-	hotpatch( reinterpret_cast<void*>( toHotpatch ),
-	          reinterpret_cast<void*>( replacement ) );
-
+	hotpatcher.join();
 	worker.join();
-	// hotpatcher.join();
 }
